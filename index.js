d7e52f5ec08fa583738344eb7525b73eb8d57872
LakelChester <lakel@chesterassociate.com>
2022-06-10 12:52:05 -0700

    commit tree 9523464955fdeb887e40de7718208c62b567e5a2

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName(){
+    customerName = customerName.toUpperCase()
+    return customerName
+
+}
+
+var bestCustomer
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+}
+
+function overwriteBestCustomer(){
+    bestCustomer = 'maybe bob'
+}
+
+const leastFavoriteCustomer = 'John'
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = "Matthew"
+    return leastFavoriteCustomer
+}
